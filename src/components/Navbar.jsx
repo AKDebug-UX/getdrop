18f54f222dfd97a48bf65054cfe0a83a11fb7ba6
upraisertech <akoredesalaudeen54@gmail.com>
2023-10-28 06:23:15 -1100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
-  const [sticky, setSticky] = useState(false);
-  const [open, setOpen] = useState(false);
 
   return (
     <nav
@@ -17,47 +15,11 @@ const Navbar = () => {
             className="w-[50px] h-[50px] object-cover bg-cyan-600 rounded-[50%]"
           />
           <h4 className="text-4xl text-bnbcolor uppercase font-bold">
-           BNB<span className="font-normal">CHAIN</span>
+            BNB<span className="font-normal">CHAIN</span>
           </h4>
         </div>
-        <div
-          className={` ${
-            sticky ? "md:bg-white/0 bg-white" : "bg-white"
-          } text-gray-900 md:block hidden px-7 py-2 font-medium  rounded-bl-full`}
-        >
-          <ul className="flex items-center gap-1 py-2 text-lg">
-            {menuLinks?.map((menu, i) => (
-              <li key={i} className="px-6 hover:text-cyan-600">
-                <a href={menu?.link}>{menu?.name}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div
-          onClick={() => setOpen(!open)}
-          className={`z-[999]  ${
-            open ? "text-gray-900" : "text-gray-100"
-          } text-3xl md:hidden m-5`}
-        >
+        <div className={`z-[999] `} >
           <ion-icon name="menu"></ion-icon>
-        </div>
-        <div
-          className={`md:hidden text-gray-900 absolute w-2/3 h-screen
-      px-7 py-2 font-medium bg-white top-0 duration-300 ${
-        open ? "right-0" : "right-[-100%]"
-      }`}
-        >
-          <ul className="flex flex-col justify-center h-full gap-10 py-2 text-lg">
-            {menuLinks?.map((menu, i) => (
-              <li
-                onClick={() => setOpen(false)}
-                key={i}
-                className="px-6 hover:text-cyan-600"
-              >
-                <a href={menu?.link}>{menu?.name}</a>
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </nav>
