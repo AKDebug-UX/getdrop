0dc36ae80182687b79c24da4a42795b1d7d664fa
upraisertech <akoredesalaudeen54@gmail.com>
2023-10-29 03:07:39 -1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,17 @@ import Footer from "./components/Footer";
 import banner from "./assets/images/banner-bottom-bg.png";
 
 const App = () => {
-  const [ration] = useState(new Date('2024-10-28'));
-  const current = new Date();
-  const isW = current > ration;
-
-
   return (
     <div className="overflow-x-hidden">
-      {isW ? (
-        <p></p>
-      ) : (
-        <>
-          <Hero />
-          <div className="bg-white">
-            <img className="" src={banner} />
-            <Ecosystem />
-            <BuildBNBChain />
-            <DappBay />
-            <Community />
-            <Footer />
-          </div>
-        </>
-      )}
+      <Hero />
+      <div className="bg-white">
+        <img className="" src={banner} />
+        <Ecosystem />
+        <BuildBNBChain />
+        <DappBay />
+        <Community />
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./Ignore";
 import "./index.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
